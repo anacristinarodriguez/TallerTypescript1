261087e699294807baaecdfa180704bdf22ef087
Ana Cristina Rodriguez Diaz <91084284+anacristinarodriguez@users.noreply.github.com>
2023-10-23 00:43:40 -0500

    Add files via upload

--- ProyectoTypescript/main.ts
+++ ProyectoTypescript/main.ts
@@ -3,15 +3,17 @@ import { series } from './data.js';
 
 let seriesTable: HTMLElement = document.getElementById("series")!;
 let promedioTemporadasElement: HTMLElement = document.getElementById("promedioTemporadas")!;
+let seriesInfo: HTMLElement = document.getElementById('seriesInfo')!;
 
 
 mostrarTablaSeries(series);
-promedio.innerHTML = `<b>Seasons average:</b> ${promedio(series)}`;
+promedioTemporadasElement.innerHTML = `<b>Seasons average:</b> ${promedio(series)}`;
 
 function mostrarTablaSeries(datosSeries: Serie[]): void {
     let seriesTbody: HTMLElement = document.createElement("tbody");
         for (let serie of datosSeries) {
             let trElement: HTMLElement = document.createElement("tr"); 
+            trElement.addEventListener("click", () => mostrarInfo(serie));
             trElement.classList.add("table-light");
             trElement.innerHTML = `<td>${serie.id}</td>
             <td>${serie.nombre}</td>
@@ -31,6 +33,19 @@ function promedio(series: Serie[]): number {
     return sum / series.length;
 }
 
+
+function mostrarInfo(serie: Serie): void {
+    seriesInfo.innerHTML = `
+        <div class="card" style="width: 19rem;">
+        <img class="card-img-top" src="${serie.imagen}"  height: auto;">
+            <div class="card-body">
+                <h1 class="card-title" style="font-size:larger">${serie.nombre}</h1>   
+                <p>${serie.descripcion}</p>
+                <p> <a href="${serie.link}" target="_blank">${serie.link}</a></p>
+            </div>
+        </div>
+    `;
+}
 
 
 
--- ProyectoTypescript/scripts/main.js
+++ ProyectoTypescript/scripts/main.js
@@ -1,22 +1,31 @@
 import { series } from './data.js';
 var seriesTable = document.getElementById("series");
+var promedioTemporadasElement = document.getElementById("promedioTemporadas");
+var seriesInfo = document.getElementById('seriesInfo');
 mostrarTablaSeries(series);
+promedioTemporadasElement.innerHTML = "<b>Seasons average:</b> ".concat(promedio(series));
 function mostrarTablaSeries(datosSeries) {
     var seriesTbody = document.createElement("tbody");
-    for (var _i = 0, datosSeries_1 = datosSeries; _i < datosSeries_1.length; _i++) {
-        var serie = datosSeries_1[_i];
+    var _loop_1 = function (serie) {
         var trElement = document.createElement("tr");
+        trElement.addEventListener("click", function () { return mostrarInfo(serie); });
         trElement.classList.add("table-light");
         trElement.innerHTML = "<td>".concat(serie.id, "</td>\n            <td>").concat(serie.nombre, "</td>\n            <td>").concat(serie.canal, "</td>\n            <td>").concat(serie.temporadas, "</td>");
         seriesTbody.appendChild(trElement);
+    };
+    for (var _i = 0, datosSeries_1 = datosSeries; _i < datosSeries_1.length; _i++) {
+        var serie = datosSeries_1[_i];
+        _loop_1(serie);
     }
     seriesTable.appendChild(seriesTbody);
 }
-//function average of seasons
-function average(series) {
+function promedio(series) {
     var sum = 0;
     series.forEach(function (serie) {
         sum += serie.temporadas;
     });
     return sum / series.length;
+}
+function mostrarInfo(serie) {
+    seriesInfo.innerHTML = "\n        <div class=\"card\" style=\"width: 19rem;\">\n        <img class=\"card-img-top\" src=\"".concat(serie.imagen, "\"  height: auto;\">\n            <div class=\"card-body\">\n                <h1 class=\"card-title\" style=\"font-size:larger\">").concat(serie.nombre, "</h1>   \n                <p>").concat(serie.descripcion, "</p>\n                <p> <a href=\"").concat(serie.link, "\" target=\"_blank\">").concat(serie.link, "</a></p>\n            </div>\n        </div>\n    ");
 }
