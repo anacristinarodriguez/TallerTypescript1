4c54a2d1d156244576040ed63a50381f45af485f
Ana Cristina Rodriguez Diaz <91084284+anacristinarodriguez@users.noreply.github.com>
2023-12-04 22:04:19 -0500

    Delete TallerTypescript1-main directory

--- TallerTypescript1-main/ProyectoTypescript/serie.ts
+++
@@ -1,4 +0,0 @@
-export class Serie {
-    constructor (public id:number, public nombre:string, public canal: string, public temporadas: number, public descripcion:string,public link:string ,public imagen:string){
-    }
-}
